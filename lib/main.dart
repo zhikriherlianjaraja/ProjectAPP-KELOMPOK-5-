0d29fed16177b8a0a3e75738c86faef297f06751
zhikriherlianjaraja <zhikriherlianjaraja>
2024-02-21 10:24:46 +0800

    commite 2

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Aplikasi pertama',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 97, 91, 107)),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 0, 0)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Projeck awal'),
     );
   }
+}
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+      appBar: AppBar(
+    title: const Text('appBar Demo'),
+    actions: <Widget>[
+      IconButton(
+        icon: const Icon(Icons.add_alert),
+        tooltip: 'Show Snackbar',
+        onPressed: () {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text('This is a Snakbar')));
+        },
+      ),
+    ],
+  ));
 }
 
 class MyHomePage extends StatefulWidget {
